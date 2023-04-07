7c4bf6f0dddde7d6d06092c2cafe02234a4a84e3
jhn-cde <140156@unsaac.edu.pe>
2023-04-07 13:42:53 -0500

    margin between components

--- components/excursion/excursionInfo/ExcursionInfo.tsx
+++ components/excursion/excursionInfo/ExcursionInfo.tsx
@@ -10,9 +10,15 @@ const ExcursionInfo = () => {
       <div className='mx-10 text-center border-b border-gray-200 sticky z-10 top-0 bg-white shadow-2xl shadow-gray-200 2xl:mx-0'>
         <Tabs ids={['Itinerary', 'Details', 'Gallery']}/>
       </div>
-      <Itinerary />
-      <Details />
-      <Gallery />
+      <div className='my-14'>
+        <Itinerary />
+      </div>
+      <div className='my-14'>
+        <Details />
+      </div>
+      <div className='my-14'>
+        <Gallery />
+      </div>
     </div>
   )
 }
