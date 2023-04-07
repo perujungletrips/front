014899ab1fea6c0410c16152562ada95814e9851
jhn-cde <140156@unsaac.edu.pe>
2023-04-07 13:45:36 -0500

    text size

--- components/layout/footer/Footer.tsx
+++ components/layout/footer/Footer.tsx
@@ -8,7 +8,7 @@ const Footer = () => {
     <div className='w-full bg-gray-900 text-white '>
       <div className='max-w-6xl m-auto flex flex-wrap justify-between items-center'>
         <div className='pt-10 md:py-10 flex flex-col items-center w-full md:w-1/3'>
-          <h6 className='text-xl mb-4'>Contact Us</h6>
+          <h6 className='text-3xl mb-4'>Contact Us</h6>
           <div>
             <p className='text-sm mb-1'><span className='font-bold'>Address: </span> Procuradores N°385</p>
             <p className='text-sm mb-1'><span className='font-bold'>Phone: </span> +51 924 353 276</p>
@@ -21,11 +21,11 @@ const Footer = () => {
             width={200}
             height={200}
             alt='footer logo'
-            className='h-36 w-auto'
+            className='w-auto h-52'
           />
         </div>
         <div className='py-10 flex flex-col items-center w-full md:w-1/3 text-xl'>
-          <h6 className='text-xl mb-4'>Visit our sites</h6>
+          <h6 className='text-3xl mb-4'>Visit our sites</h6>
           <div className='w-full flex felx-row justify-center'>
             <button type='button' className='mx-2 hover:text-white/80'>
               <FaInstagram />
